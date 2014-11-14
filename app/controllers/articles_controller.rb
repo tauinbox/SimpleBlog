493b2fc1bf9f271389079e1753c3f7f9d6e23f2b
tauinbox <tauinbox@gmail.com>
2014-11-15 00:08:11 +0300

    Remove excess check

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -25,14 +25,7 @@ class ArticlesController < ApplicationController
 
   def edit
     @article = Article.find(params[:id])
-    if user_signed_in?
-      if current_user.id != @article.user_id
-        flash.now[:error] = "YOU CAN'T EDIT OTHER PEOPLE'S POSTS!"
-        @articles = Article.all
-        @users = User.all
-        render action: 'index'
-      end
-    else
+    if current_user.id != @article.user_id
       flash.now[:error] = "YOU CAN'T EDIT OTHER PEOPLE'S POSTS!"
       @articles = Article.all
       @users = User.all
