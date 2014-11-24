21cf4a0349e96e470789e310d73db28df222a690
tauinbox <tauinbox@gmail.com>
2014-11-24 20:37:21 +0300

    Add tests to check the size of the title and text

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -4,11 +4,14 @@ describe Article do
   describe "validations" do
     it { should validate_presence_of :title }
     it { should validate_presence_of :text }
+    it { should ensure_length_of(:title).is_at_most(140) }
+    it { should ensure_length_of(:text).is_at_most(4000) }
   end
 
   describe "associations" do
     it { should have_many :comments }
   end
+
 
   describe "#subject" do
     it "returns the article title" do
