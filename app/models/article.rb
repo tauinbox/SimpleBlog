d7596ab48b1936540da3d3c43a22c49491e09d5e
tauinbox <tauinbox@gmail.com>
2014-11-24 20:36:32 +0300

    Add validations to check the size of the title and text

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
-  validates :title, presence: true
-  validates :text, presence: true
+  validates :title, presence: true, length: { maximum: 140 }
+  validates :text, presence: true, length: { maximum: 4000 }
 
   has_many :comments
   belongs_to :user
