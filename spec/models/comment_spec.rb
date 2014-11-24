ff205dac4dff7d2206ef2a4b424ab7e032b2a837
Alexander Tupavov <tauinbox@gmail.com>
2014-11-24 21:18:42 +0300

    Add comment length test

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,8 @@
 require "spec_helper"
 
 describe Comment do
-  it { should belong_to :article }
+  describe "validations" do
+    it { should belong_to :article }
+    it { should ensure_length_of(:body).is_at_most(4000) }
+  end
 end
