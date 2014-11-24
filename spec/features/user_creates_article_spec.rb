e5192ef65addec42b93e17b7b82e3ab4203e7aaf
Alexander Tupavov <tauinbox@gmail.com>
2014-11-24 21:19:50 +0300

    Add article creation by user test

--- spec/features/user_creates_article_spec.rb
+++ spec/features/user_creates_article_spec.rb
@@ -1,12 +1,23 @@
 require "spec_helper"
 
 feature "Article Creation" do
-  before(:all) do
+  before(:each) do
     sign_up
   end
 
   scenario "allows user to visit new article page" do
     visit new_article_path    
     expect(page).to have_content I18n.t('articles.new')
+  end
+
+  scenario "allows user to create new article" do
+    visit new_article_path
+
+    fill_in :article_title, :with => "Test Article Title"
+    fill_in :article_text, :with => "Content for new article"
+
+    click_button I18n.t('articles.create')
+
+    expect(page).to have_content 'Test Article Title'
   end
 end
