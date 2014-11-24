9da6334a76a1c529eaff2bdf81f81a5ac0ff792b
Alexander Tupavov <tauinbox@gmail.com>
2014-11-24 17:18:31 +0300

    refactoring syntax code for hashe assignment (:hash => to hash:)

--- spec/support/session_helper.rb
+++ spec/support/session_helper.rb
@@ -1,10 +1,10 @@
 def sign_up
     visit new_user_registration_path
 
-    fill_in :user_email, :with => 'user@tester.com'
-    fill_in :user_username, :with => 'mike123'
-    fill_in :user_password, :with => 'secure123!@#'
-    fill_in :user_password_confirmation, :with => 'secure123!@#'
+    fill_in :user_email, with: 'user@tester.com'
+    fill_in :user_username, with: 'mike123'
+    fill_in :user_password, with: 'secure123!@#'
+    fill_in :user_password_confirmation, with: 'secure123!@#'
 
     click_button I18n.t('buttons.sign_up')
 end
