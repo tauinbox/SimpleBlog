ed4dabd770fb174118c88b4bc3a142e672ac1f13
Alexander Tupavov <tauinbox@gmail.com>
2014-12-10 14:20:47 +0300

    remove bad migration

--- db/migrate/20141209063310_add_rating_fields_to_article_comment_profile.rb
+++
@@ -1,7 +0,0 @@
-class AddRatingFieldsToArticleCommentProfile < ActiveRecord::Migration
-  def change
-    add_column :articles, :rating, :integer, default: 0
-    add_column :comments, :rating, :integer, default: 0
-    add_column :profiles, :rating, :integer, default: 0
-  end
-end
