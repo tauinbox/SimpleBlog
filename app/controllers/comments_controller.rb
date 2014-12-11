d7e22c49664437e7aec7dd16c88d4dea27ec76f6
Alexander Tupavov <tauinbox@gmail.com>
2014-12-11 10:15:20 +0300

    Add methods for voting

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,5 @@
 class CommentsController < ApplicationController
-  before_filter :authenticate_user!, :only => [:create]
+  before_filter :authenticate_user!, :only => [:create, :voteup, :votedown]
 
   def create
     @article = Article.find(params[:article_id])
@@ -7,6 +7,47 @@ class CommentsController < ApplicationController
     @comment.save
 
     redirect_to article_path(@article)
+  end
+
+   def voteup
+    @article = Article.find(params[:article_id])
+    @comment = Comment.find(params[:id])
+    # flash[:notice] = @article.votes.where(user_id: current_user.id).first.inspect
+    unless vote = @comment.votes.where(user_id: current_user.id).first
+      vote = @comment.votes.new
+      vote.user = current_user
+    end
+    if vote.value == 1
+      flash[:notice] = 'Alredy voted'
+    else
+      vote.value += 1
+      vote.save
+    end
+    #flash[:notice] = vote.inspect
+    redirect_to @article
+  end
+
+   def votedown
+    @article = Article.find(params[:article_id])
+    @comment = Comment.find(params[:id])
+    # flash[:notice] = @article.votes.where(user_id: current_user.id).first.inspect
+    unless vote = @comment.votes.where(user_id: current_user.id).first
+      vote = @comment.votes.new
+      vote.user = current_user
+    end
+    if vote.value == -1
+      flash[:notice] = 'Alredy voted'
+    else
+      vote.value -= 1
+      vote.save
+    end
+    #flash[:notice] = vote.inspect
+    redirect_to @article
+  end
+
+  def votemap
+    @comment = Comment.find(params[:id])
+    @votes = @comment.votes
   end
 
   private
