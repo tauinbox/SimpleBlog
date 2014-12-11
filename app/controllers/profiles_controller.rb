131b53fa47ebe9721f6fd76243e3dc781b6abf9a
Alexander Tupavov <tauinbox@gmail.com>
2014-12-11 10:15:47 +0300

    Add methods for voting

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,5 @@
 class ProfilesController < ApplicationController
+  before_filter :authenticate_user!, :only => [:edit, :update, :voteup, :votedown]
 
   def show
     @profile = Profile.where(user_id: params[:user_id]).first
@@ -22,6 +23,49 @@ class ProfilesController < ApplicationController
     else
       render action: 'edit'
     end
+  end
+
+  def voteup
+    @profile = Profile.where(user_id: params[:user_id]).first
+    # flash[:notice] = @article.votes.where(user_id: current_user.id).first.inspect
+    unless @profile.user == current_user
+      unless vote = @profile.votes.where(user_id: current_user.id).first
+        vote = @profile.votes.new
+        vote.user = current_user
+      end
+      if vote.value == 1
+        flash[:notice] = 'Alredy voted'
+      else
+        vote.value += 1
+        vote.save
+      end
+    end
+    #flash[:notice] = vote.inspect
+    redirect_to action: 'show'
+  end
+
+  def votedown
+    @profile = Profile.where(user_id: params[:user_id]).first
+    # flash[:notice] = @article.votes.where(user_id: current_user.id).first.inspect
+    unless @profile.user == current_user
+      unless vote = @profile.votes.where(user_id: current_user.id).first
+        vote = @profile.votes.new
+        vote.user = current_user
+      end
+      if vote.value == -1
+        flash[:notice] = 'Alredy voted'
+      else
+        vote.value -= 1
+        vote.save
+      end
+    end
+    #flash[:notice] = vote.inspect
+    redirect_to action: 'show'
+  end
+
+  def votemap
+    @profile = Profile.where(user_id: params[:user_id]).first
+    @votes = @profile.votes
   end
 
   private
