5e76dacc5cf67b85101c24a9e9d1f4ffb9a49da6
Alexander Tupavov <tauinbox@gmail.com>
2015-01-22 21:44:57 +0300

    change article and comments  vote methods from POST to GET

--- config/routes.rb
+++ config/routes.rb
@@ -22,14 +22,14 @@ Rails.application.routes.draw do
   resources :articles do
     resources :comments, only: :create do
       member do
-        post :voteup
-        post :votedown
+        get :voteup
+        get :votedown
         get :votemap      
       end
     end
     member do
-      post :voteup
-      post :votedown
+      get :voteup
+      get :votedown
       get :votemap
     end
   end
