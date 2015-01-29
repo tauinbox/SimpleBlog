1633551e062ac1a53b4d4807308a4d5b0a28687b
Alexander Tupavov <tauinbox@gmail.com>
2015-01-29 19:55:52 +0300

    Add child object dependencies to Article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,10 +2,10 @@ class Article < ActiveRecord::Base
   validates :title, presence: true, length: { maximum: 140 }
   validates :text, presence: true, length: { maximum: 4000 }
 
-  has_many :comments
+  has_many :comments, dependent: :destroy
   belongs_to :user
-  has_many :images, as: :imageable
-  has_many :votes, as: :voting
+  has_many :images, as: :imageable, dependent: :destroy
+  has_many :votes, as: :voting, dependent: :destroy
   has_many :users, through: :votes
 
   accepts_nested_attributes_for :images
