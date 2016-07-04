795cb50fec9f37e1dcb786e99f712bd833e4cbe7
Alexander Tupavov <tauinbox@gmail.com>
2016-07-04 18:44:28 +0300

    correct maximum article length

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ActiveRecord::Base
   validates :title, presence: true, length: { maximum: 140 }
-  validates :text, presence: true, length: { maximum: 4000 }
+  validates :text, presence: true, length: { maximum: 200000 }
 
   has_many :comments, dependent: :destroy
   belongs_to :user
